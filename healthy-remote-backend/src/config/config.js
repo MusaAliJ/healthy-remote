3189d9a4db7deb93c0097b71512b10f6f8082692
macbook <musajprogrammer@gmail.com>
2022-10-15 13:45:32 +0500

    new

--- healthy-remote-backend/src/config/config.js
+++ healthy-remote-backend/src/config/config.js
@@ -1,6 +1,7 @@
 export const config = {
   secrets: {
-    jwt: "healthy-remote"
+    jwt: "healthy-remote",
+    new: "d"
   },
   dbUrl:
     "mongodb+srv://musa:admin123@cluster0.ha3f4mc.mongodb.net/?retryWrites=true&w=majority"

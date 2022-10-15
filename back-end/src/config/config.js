dd9748027b4650d5969c63c2308f73c4cc988fdf
osamafarooq45 <muhammadosama@brandver.se>
2022-10-15 16:50:17 +0500

    Added notification for whatsapp

--- back-end/src/config/config.js
+++ back-end/src/config/config.js
@@ -2,6 +2,18 @@ export const config = {
   secrets: {
     jwt: "healthy-remote"
   },
+  whatsapp: {
+    options: {
+      method: "POST",
+      hostname: "api.ultramsg.com",
+      port: null,
+      path: "/instance20195/messages/chat",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded"
+      }
+    },
+    token: "qt5timsu5vregcia"
+  },
   dbUrl:
     "mongodb+srv://musa:admin123@cluster0.ha3f4mc.mongodb.net/?retryWrites=true&w=majority"
 }
